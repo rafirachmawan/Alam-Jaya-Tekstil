46ef534a167f2582f9f5c86b7b7b3c5b32d420f8
bangkit16 <bangkitcaniago@gmail.com>
2026-04-09 16:35:35 +0700

    fix prisma

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
