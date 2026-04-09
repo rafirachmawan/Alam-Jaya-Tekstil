f4720a17d5c1fa455f751e9a7722100f8bb3c4d1
bangkit16 <bangkitcaniago@gmail.com>
2026-04-10 00:01:41 +0700

    fix prisma generate

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,10 +1,16 @@
 
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export { prisma };
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export { prisma };
